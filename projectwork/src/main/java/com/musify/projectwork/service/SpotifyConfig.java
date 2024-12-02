a79171051e1b6412ee4b25583b4a520e9770a4d7
Robka03 <liptakrobi03@gmail.ccom>
2024-12-02 19:47:32 +0100

    dotenv fix

--- projectwork/src/main/java/com/musify/projectwork/service/SpotifyConfig.java
+++ projectwork/src/main/java/com/musify/projectwork/service/SpotifyConfig.java
@@ -1,16 +1,20 @@
 package com.musify.projectwork.service;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @Component
 public class SpotifyConfig {
 
-    @Value("${SPOTIFY_CLIENT_ID}")
-    private String clientId;
+    Dotenv dotenv = Dotenv.configure()
+            .directory("./")
+            .load();
 
-    @Value("${SPOTIFY_CLIENT_SECRET}")
-    private String clientSecret;
+    private String clientId = dotenv.get("SPOTIFY_CLIENT_ID");
+
+    // @Value("${SPOTIFY_CLIENT_SECRET}")
+    private String clientSecret =dotenv.get("SPOTIFY_CLIENT_SECRET");
 
     public String getClientId() {
         return clientId;
