24b4a3ea0fc7aab4109d14532d53af71100a4736
bit-creepy <attilamolnar4488@gmail.com>
2024-12-05 22:55:41 +0100

    test
    Frontend merge

--- projectwork/frontend/src/components/logout/Logout.tsx
+++ projectwork/frontend/src/components/logout/Logout.tsx
@@ -10,6 +10,7 @@ export default function Logout() {
         localStorage.removeItem("user");
         setIsLoggedIn(false);
         setUser(null);
+        
         navigate("/");
     },[])
     return (
